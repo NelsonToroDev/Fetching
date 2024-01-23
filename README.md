3c279cb2311791879ae761b5d9c52be0462f8833
Nelson Toro <nelson.toro.flores@gmail.com>
2024-01-23 19:30:57 -0400

    Implemented useEffect with two data fetching one for random fact and another for image tags

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 React Technical Practice based on Midudev training course
 https://www.youtube.com/playlist?list=PLUofhDIg_38q4D0xNWp7FEHOTcZhjWJ29
 
-Create a page using the first API to retrieve a random fact and using the second API retrieve an random image sending the first three words from the fact
+Create a page using the first API to retrieve a random fact and using the second API retrieve an random image sending the first three words from the fact as well as the tags describing the image
 
 To accomplish it you should use the following APIs:
 
 - Facts Random: https://catfact.ninja/fact
-- Imagen random: https://cataas.com/cat/says/hello
+- Imagen random: https://cataas.com/cat/says/${threeFirstWords}
+-- Endpointto get tags `https://cataas.com/cat/says/${threeFirstWords}?json=true`
 
 Practice:
+- fetch
 - useState hook
 - useEffect hook
 - Custom hooks
--- app.jsx
+++ app.jsx
@@ -1,3 +1,40 @@
+import { useEffect, useState } from 'react'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${hello}?json=true`
+
 export function App() {
-  return <h1>App de Gatitos</h1>
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
+  const [tags, setTags] = useState()
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((res) => res.json())
+      .then((data) => {
+        setFact(data.fact)
+        const threeFirstWords = data.fact.split(' ', 3).join(' ')
+        setImageUrl(`https://cataas.com/cat/says/${threeFirstWords}`)
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?json=true`)
+          .then((res) => res.json())
+          .then((data) => {
+            const { tags } = data
+            const newTags = tags && tags.length >= 0 ? tags.join(' ') : undefined
+            setTags(newTags)
+          })
+      })
+  }, [])
+
+  return (
+    <main>
+      <h1>Kitty App</h1>
+      {fact && <p>{fact}</p>}
+      {tags && <p>tags: {tags}</p>}
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt={`Image retrived from the first three words of the fact: ${fact}`}
+        />
+      )}
+    </main>
+  )
 }
