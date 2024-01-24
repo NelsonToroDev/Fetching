f34ce6918883b616b00d1a5d0bb967cd73e64b41
Nelson Toro <nelson.toro.flores@gmail.com>
2024-01-23 22:46:29 -0400

    Fix joining tags

--- app.jsx
+++ app.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${hello}?json=true`
 
-export function App() {
+export function App () {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
   const [tags, setTags] = useState()
@@ -18,7 +18,8 @@ export function App() {
           .then((res) => res.json())
           .then((data) => {
             const { tags } = data
-            const newTags = tags && tags.length >= 0 ? tags.join(' ') : undefined
+            const newTags =
+              tags && tags.length >= 0 ? tags.join(' ') : undefined
             setTags(newTags)
           })
       })
